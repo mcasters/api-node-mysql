1a8de7e9084d2d558b5fe0a32a4f11732be05883
Unknown <yonk@wanadoo.fr>
2020-09-15 00:24:08 +0200

    Access control origin

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const host = 'api.localhost';
-const port = 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
+
 const app = express();
 
 // parse requests of content-type: application/json
@@ -11,6 +13,9 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Access-Control-Allow-Origin
+app.use(cors());
 
 // simple route
 app.get("/", (req, res) => {
@@ -23,6 +28,6 @@ require("./app/routes/sculpture.routes.js")(app);
 require("./app/routes/content.routes.js")(app);
 require("./app/routes/user.routes.js")(app);
 
-app.listen(port, () => {
+app.listen(`${port}`, () => {
     console.log(`Server is running on http://${host}:${port}/`);
 });
